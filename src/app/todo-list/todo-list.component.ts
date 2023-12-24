bb330bb3dd448372d5d2d8c1765998b166300df0
Travis Craft <tlcraft@users.noreply.github.com>
2023-12-24 11:34:28 -0700

    Add initial todo list.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
-
+  todoList: string[] = ["Item"];
 }
