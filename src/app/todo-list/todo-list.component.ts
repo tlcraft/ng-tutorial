046c62b22e91f334dd6e74574c886f1985286a06
Travis Craft <tlcraft@users.noreply.github.com>
2023-12-26 12:24:42 -0700

    Create add item method.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,13 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
   todoList: string[] = ["Item"];
+  newItem = new FormControl('');
+
+  addItem(): void {
+    console.log("Add: ", this.newItem.value);
+  }
 }
