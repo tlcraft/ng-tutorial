8139a16d631b6cde6b4ce42e802fc06e85ad2313
Travis Craft <tlcraft@users.noreply.github.com>
2024-01-13 14:50:52 -0700

    Add item component.

---
+++ src/app/todo-item/todo-item.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.scss'
+})
+export class TodoItemComponent {
+  @Input() item: { name: string, id: string };
+  @Output() removeEmitter: EventEmitter<any> = new EventEmitter();
+
+  removeItem(id: string): void {
+    this.removeEmitter.emit();
+  }
+}
