b019842cece7611d7703fef385c5256345649246
Travis Craft <tlcraft@users.noreply.github.com>
2024-01-14 11:54:26 -0700

    Extract methods and use component.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -2,23 +2,24 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { v4 as uuidv4 } from 'uuid';
+import { TodoItemComponent } from '../todo-item/todo-item.component';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, TodoItemComponent],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss'
 })
 export class TodoListComponent {
-  todoList: { name: string, id: string}[] = [];
+  todoList: { name: string, id: string }[] = [];
   newItem = new FormControl('');
 
   addItem(): void {
     const newItem = this.newItem.value;
 
     if (newItem) {
-      this.todoList.push({ name: newItem, id:uuidv4()});
+      this.todoList.push({ name: newItem, id: uuidv4() });
     }
   }
 
