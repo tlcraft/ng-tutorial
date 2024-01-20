ffcd7f33a8d3e835ea20447ddc5cb534fa1622a5
Travis Craft <tlcraft@users.noreply.github.com>
2024-01-19 19:00:53 -0700

    Add initial save button.

--- src/app/todo-item/todo-item.component.ts
+++ src/app/todo-item/todo-item.component.ts
@@ -1,14 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-todo-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './todo-item.component.html',
   styleUrl: './todo-item.component.scss'
 })
 export class TodoItemComponent {
-  @Input() item: { name: string, id: string };
+  @Input() item: { name: string, id: string, inEditMode: boolean };
   @Output() removeEmitter: EventEmitter<any> = new EventEmitter();
 
   removeItem(id: string): void {
@@ -17,5 +18,11 @@ export class TodoItemComponent {
 
   editItem(id: string): void {
     console.log("Edit: ", id);
+    this.item.inEditMode = true;
+  }
+
+  saveItem(id: string): void {
+    console.log("Save: ", id);
+    this.item.inEditMode = false;
   }
 }
