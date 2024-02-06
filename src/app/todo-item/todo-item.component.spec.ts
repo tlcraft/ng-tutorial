214c6d4dc24d911df8fa6fa48624c72173a8a4d7
Travis Craft <tlcraft@users.noreply.github.com>
2024-02-05 19:03:43 -0700

    Update item tests.

--- src/app/todo-item/todo-item.component.spec.ts
+++ src/app/todo-item/todo-item.component.spec.ts
@@ -5,6 +5,10 @@ import { TodoItemComponent } from './todo-item.component';
 describe('TodoItemComponent', () => {
   let component: TodoItemComponent;
   let fixture: ComponentFixture<TodoItemComponent>;
+  const mockItem = { 
+    id: "1234abc",
+    name: "item"
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +18,14 @@ describe('TodoItemComponent', () => {
     
     fixture = TestBed.createComponent(TodoItemComponent);
     component = fixture.componentInstance;
+    component.item = mockItem;
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+    expect(component.item.id).toBe('1234abc');
+    expect(component.item.name).toBe('item');
+    expect(component.inEditMode).toBeUndefined();
   });
 });
