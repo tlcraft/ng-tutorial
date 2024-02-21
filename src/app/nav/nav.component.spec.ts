3f2a15a7f61747575d959e35eb01eb0ce9158330
Travis Craft <tlcraft@users.noreply.github.com>
2024-02-20 18:04:12 -0700

    Add nav test.

--- src/app/nav/nav.component.spec.ts
+++ src/app/nav/nav.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NavComponent } from './nav.component';
+import { ActivatedRoute } from '@angular/router';
 
 describe('NavComponent', () => {
   let component: NavComponent;
   let fixture: ComponentFixture<NavComponent>;
 
   beforeEach(async () => {
+    const mockActivedRoute = {};
+
     await TestBed.configureTestingModule({
-      imports: [NavComponent]
+      imports: [NavComponent],
+      providers: [{provide: ActivatedRoute, useValue: mockActivedRoute }]
     })
     .compileComponents();
     
@@ -19,5 +23,10 @@ describe('NavComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render navigation', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('a')?.textContent).toContain('Home');
   });
 });
