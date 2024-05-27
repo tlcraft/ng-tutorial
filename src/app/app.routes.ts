84cf8fd8e4e57830de96b219fb9490df8f49ddcf
Travis Craft <tlcraft@users.noreply.github.com>
2024-05-27 11:44:37 -0600

    Add default route.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { TodoListComponent } from './todo-list/todo-list.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
+    {path: '', component: HomeComponent},
     {path: 'home', component: HomeComponent},
     {path: 'todo-list', component: TodoListComponent},
 ];
