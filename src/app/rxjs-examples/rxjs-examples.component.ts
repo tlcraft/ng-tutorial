1087c08b235f63f340587cf23cde8e0acc13133d
Travis Craft <tlcraft@users.noreply.github.com>
2024-05-30 18:23:11 -0600

    Add initial component.

---
+++ src/app/rxjs-examples/rxjs-examples.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-rxjs-examples',
+  standalone: true,
+  imports: [],
+  templateUrl: './rxjs-examples.component.html',
+  styleUrl: './rxjs-examples.component.scss'
+})
+export class RxjsExamplesComponent {
+
+}
