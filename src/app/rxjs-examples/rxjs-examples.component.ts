71b8df0e6eba9c0c6d85e9897ba2eeb02a0b6ad7
Travis Craft <tlcraft@users.noreply.github.com>
2024-06-02 11:24:43 -0600

    Add interval example.

--- src/app/rxjs-examples/rxjs-examples.component.ts
+++ src/app/rxjs-examples/rxjs-examples.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { interval, take } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs-examples',
@@ -7,6 +8,11 @@ import { Component } from '@angular/core';
   templateUrl: './rxjs-examples.component.html',
   styleUrl: './rxjs-examples.component.scss'
 })
-export class RxjsExamplesComponent {
+export class RxjsExamplesComponent implements OnInit {
+  numbers = interval(1000);
+  takeFourNumbers = this.numbers.pipe(take(4));
 
+  ngOnInit() {
+     this.takeFourNumbers.subscribe(x => console.log('Next: ', x));
+  }
 }
