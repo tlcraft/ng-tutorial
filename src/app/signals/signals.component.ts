a0601b09be80bf15c9377f7912ec1a62b1283c7e
Travis Craft <tlcraft@users.noreply.github.com>
2024-07-29 19:11:08 -0600

    Add signals component.

---
+++ src/app/signals/signals.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signals',
+  standalone: true,
+  templateUrl: './signals.component.html',
+  styleUrl: './signals.component.scss'
+})
+export class SignalsComponent implements OnInit {
+  ngOnInit() {
+    
+  }
+}
