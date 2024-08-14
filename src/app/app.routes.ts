7f96b8f2288309fdf2ba37a63ba546f74e101b93
Travis Craft <tlcraft@users.noreply.github.com>
2024-08-14 15:36:35 -0600

    Edit name.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,5 +9,5 @@ export const routes: Routes = [
     {path: 'home', component: HomeComponent},
     {path: 'todo-list', component: TodoListComponent},
     {path: 'rxjs', component: RxjsExamplesComponent},
-    {path: 'signals', component: SignalsComponent, data: { firstName: 'First', lastName: 'Last'} }
+    {path: 'signals', component: SignalsComponent, data: { firstName: 'John', lastName: 'Doe'} }
 ];
