1acd03130ade6a0ba7895d5fad91c5e9db8309cf
Travis Craft <tlcraft@users.noreply.github.com>
2024-09-05 17:30:37 -0600

    Update log service.

--- src/services/log.service.ts
+++ src/services/log.service.ts
@@ -2,12 +2,8 @@ import { Injectable } from "@angular/core";
 
 @Injectable({  providedIn: 'root' })
 export class Logger {
-    log(msg: unknown, data?: unknown) { 
-        if (data !== 'undefined') {
-            console.log(msg, data);
-        } else {
-            console.log(msg);
-        }
+    log(msg: unknown, ...data: unknown[]) { 
+        console.log(msg, ...data);
     }
     error(msg: unknown) { console.error(msg); }
     warn(msg: unknown) { console.warn(msg); }
