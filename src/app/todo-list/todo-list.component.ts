3afbf6f6540aa3afb36e326b4f147a4adc986b62
Travis Craft <tlcraft@users.noreply.github.com>
2024-09-06 18:33:14 -0600

    Add toggle to list page.

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -4,6 +4,7 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { v4 as uuidv4 } from 'uuid';
 import { TodoItemComponent } from '../todo-item/todo-item.component';
 import { Logger } from '../../services/log.service';
+import { ModeService } from '../../services/mode-service';
 
 @Component({
   selector: 'app-todo-list',
@@ -15,6 +16,7 @@ export class TodoListComponent implements OnInit {
   todoList: { name: string, id: string }[] = [];
   newItem = new FormControl('');
   logger = inject(Logger);
+  modeService = inject(ModeService);
 
   ngOnInit(): void {
     this.logger.log('Loading the Todo page.');
@@ -34,5 +36,10 @@ export class TodoListComponent implements OnInit {
 
   removeItem(id: string): void {
     this.todoList = this.todoList.filter(item => item.id !== id);
+  }
+
+  toggleMode() {
+    this.modeService.setMode(!this.modeService.getMode());
+    this.logger.log('Mode: ', this.modeService.getMode());
   }
 }
